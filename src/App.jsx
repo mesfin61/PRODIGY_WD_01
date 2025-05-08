b43846054d97798aa25e37f60c02b8c9482ec9c3
Mesfin Ayele <mesfinayele499@gmail.com>
2025-05-08 15:24:30 +0300

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import './App.css';
 import Banner from './components/Banner';
 import About from './components/About';
-import Featured from './components/featured';
+import Featured from './components/Featured';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
